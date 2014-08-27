96506790dde5bd1320814c5b9c72c430575999a2
Andres Mora <mora1472@gmail.com>
2014-08-27 10:54:41 -0600

    Aqui se agregó INITIAL, que es un respaldo del código original funcionando a una velocidad decente!

--- CodigoInicialExperimento2/Module_ROM.v
+++ CodigoInicialExperimento2/Module_ROM.v
@@ -17,31 +17,31 @@ module ROM
 );	
 always @ ( iAddress )
 begin
-/*
+
 	case (iAddress)
 
-//	0: oInstruction = { `NOP ,24'd4000    };
-//	1: oInstruction = { `STO , `R7,16'b0001 };
-//	2: oInstruction = { `STO ,`R3,16'h1     }; 
-//	3: oInstruction = { `STO, `R4,16'd1000 };
-//	4: oInstruction = { `STO, `R5,16'd0     };  //j
-////LOOP2:
-//	5: oInstruction = { `LED ,8'b0,`R7,8'b0 };
-//	6: oInstruction = { `STO ,`R1,16'h0     }; 	
-//	7: oInstruction = { `STO ,`R2,16'd05000 };
-////LOOP1:	
-//	8: oInstruction = { `ADD ,`R1,`R1,`R3    }; 
-//	9: oInstruction = { `BLE ,`LOOP1,`R1,`R2 }; 
-//	
-//	10: oInstruction = { `ADD ,`R5,`R5,`R3    };
-//	11: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };	
-//	12: oInstruction = { `NOP ,24'd4000       }; 
-//	13: oInstruction = { `ADD ,`R7,`R7,`R3    };
-//	14: oInstruction = { `JMP ,  8'd2,16'b0   };
-//	default:
-//		oInstruction = { `LED ,  24'b10101010 };		//NOP
-	endcase
+	0: oInstruction = { `NOP ,24'd4000    };
+	1: oInstruction = { `STO , `R7,16'b0001 };
+	2: oInstruction = { `STO ,`R3,16'h1     }; 
+	3: oInstruction = { `STO, `R4,16'd1000 };
+	4: oInstruction = { `STO, `R5,16'd0     };  //j
+//LOOP2:
+	5: oInstruction = { `LED ,8'b0,`R7,8'b0 };
+	6: oInstruction = { `STO ,`R1,16'h0     }; 	
+	7: oInstruction = { `STO ,`R2,16'd05000 };
+//LOOP1:	
+	8: oInstruction = { `ADD ,`R1,`R1,`R3    }; 
+	9: oInstruction = { `BLE ,`LOOP1,`R1,`R2 }; 
 	
+	10: oInstruction = { `ADD ,`R5,`R5,`R3    };
+	11: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };	
+	12: oInstruction = { `NOP ,24'd4000       }; 
+	13: oInstruction = { `ADD ,`R7,`R7,`R3    };
+	14: oInstruction = { `JMP ,  8'd2,16'b0   };
+	default:
+		oInstruction = { `LED ,  24'b10101010 };		//NOP
+	endcase
+	/*
 	case (iAddress)
 	
 	0: oInstruction = { `NOP , 24'd4000  };
@@ -85,6 +85,7 @@ begin
 	
 
 	*/
+	/*
 	// Verificación del pipeline
 	case (iAddress)
 
@@ -113,7 +114,7 @@ begin
 		oInstruction = 28'd0;		//NOP
 	endcase	
 	
-	
+	*/
 
 
 
---
+++ INITIAL/Readme.txt
@@ -0,0 +1,1 @@
+Estas carpetas y codigo lo que tienen es la primera corrida limpia del codigo del profe, con la velocidad alterada para ver que la suma funciona correctamente.
